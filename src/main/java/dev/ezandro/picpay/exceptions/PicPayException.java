3e7b415c04a8410c0ce52b12635e174162bae2fb
ezbueno <ezandrobueno@yahoo.com.br>
2024-08-02 23:05:06 -0300

    Add PicPayException base exception class

---
+++ src/main/java/dev/ezandro/picpay/exceptions/PicPayException.java
@@ -0,0 +1,12 @@
+package dev.ezandro.picpay.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class PicPayException extends RuntimeException {
+    public ProblemDetail toProblemDetail() {
+        var problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        problemDetail.setTitle("PicPay internal server error");
+        return problemDetail;
+    }
+}

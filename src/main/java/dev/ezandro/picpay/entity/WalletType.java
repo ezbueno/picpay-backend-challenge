950e52c7c688222f8539d4c0beb9f0667f99186e
ezbueno <ezandrobueno@yahoo.com.br>
2024-08-02 21:25:53 -0300

    Initial commit

---
+++ src/main/java/dev/ezandro/picpay/entity/WalletType.java
@@ -0,0 +1,35 @@
+package dev.ezandro.picpay.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_wallet_type")
+public class WalletType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public WalletType() {
+    }
+
+    public WalletType(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

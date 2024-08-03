5de4d0518100d8153776c068321594837409d539
ezbueno <ezandrobueno@yahoo.com.br>
2024-08-02 23:14:58 -0300

    Create WalletController to expose wallet API endpoints

---
+++ src/main/java/dev/ezandro/picpay/controllers/WalletController.java
@@ -0,0 +1,26 @@
+package dev.ezandro.picpay.controllers;
+
+import dev.ezandro.picpay.dtos.CreateWalletDTO;
+import dev.ezandro.picpay.entities.Wallet;
+import dev.ezandro.picpay.services.WalletService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+public class WalletController {
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @PostMapping(name = "/wallets")
+    public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletDTO createWalletDTO) {
+        var wallet = this.walletService.createWallet(createWalletDTO);
+        return ResponseEntity.created(URI.create("/wallets/" + wallet.getId())).body(wallet);
+    }
+}
